b5d8a0da6e43fcf8cb7a61ed40115b18ad8d9d99
b0921319248-gif <b0921319248@gmail.com>
2026-05-19 21:04:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,6 @@ with l:
 with r:
     for i in st.session_state.mylist:
          with st.container(border=True):
-             col1, col2 = st.columns(2)
+             col1, col2 = st.columns(1)
              st.write(i)
        
